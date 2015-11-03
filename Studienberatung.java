ca0819f308f901390df9aae974e5a7517d106701
Nikolai Reinke <nikolai.reinke@haw-hamburg.de>
2015-11-03 17:17:43 +0100

    Added scanner

--- Studienberatung.java
+++ Studienberatung.java
@@ -2,8 +2,10 @@ import java.util.*;
 
 public class Studienberatung {
 	public static void main (String[] args) {
-	
-	
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Hast du Abitur?");
+		String antwort = scanner.nextLine();
 	}
 
 
